ba73c9dcc3dac76865365b9596091e250fef1eca
songgz <sgzhe@163.com>
2023-09-08 10:28:26 +0800

    first commit

---
+++ lib/wizcsv/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Wizcsv
+  VERSION = "0.1.0"
+end
---
+++ test/test_wizcsv.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestWizcsv < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Wizcsv::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
